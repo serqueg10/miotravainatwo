6d5893c83cd7b25706c32777cbb1c0ea86302366
Serqueg10 <sergiogr9506@gmail.com>
2013-09-04 09:40:30 -0500

    parte dos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miotravainatwo::Application.config.secret_key_base = '4467c67ac80a1e9ee7714d4dd55e7bc632d91efcce8ecde30ae20f7c035c1665e2c0321f4a121e4fe254849846d4c1635fc13706abcaf57d4d7c312ac26a4847'
